8e4111d198d7560218b193aaae6c3b36b1422432
romualdDebruyne <romuald.debruyne@imt-atlantique.fr>
2025-02-18 11:16:14 +0100

    Maj Produits et filiere par defaut

--- src/abstraction/eqXRomu/filiere/FiliereParDefaut.java
+++ src/abstraction/eqXRomu/filiere/FiliereParDefaut.java
@@ -1,7 +1,5 @@
 package abstraction.eqXRomu.filiere;
 
-import java.util.HashMap;
-
 import abstraction.eq1Producteur1.Producteur1;
 import abstraction.eq2Producteur2.Producteur2;
 import abstraction.eq3Producteur3.Producteur3;
@@ -11,16 +9,17 @@ import abstraction.eq6Transformateur3.Transformateur3;
 import abstraction.eq7Distributeur1.Distributeur1;
 import abstraction.eq8Distributeur2.Distributeur2;
 import abstraction.eq9Distributeur3.Distributeur3;
-import abstraction.eqXRomu.acteurs.Romu;
+import abstraction.eqXRomu.acteurs.DistributeurX;
 import abstraction.eqXRomu.acteurs.ProducteurX;
+import abstraction.eqXRomu.acteurs.Romu;
 import abstraction.eqXRomu.acteurs.TransformateurX;
 import abstraction.eqXRomu.appelDOffre.SuperviseurVentesAO;
-import abstraction.eqXRomu.acteurs.DistributeurX;
 import abstraction.eqXRomu.bourseCacao.BourseCacao;
 import abstraction.eqXRomu.clients.ClientFinal;
 import abstraction.eqXRomu.contratsCadres.SuperviseurVentesContratCadre;
 import abstraction.eqXRomu.encheres.SuperviseurVentesAuxEncheres;
 import abstraction.eqXRomu.produits.Chocolat;
+import java.util.HashMap;
 
 
 public class FiliereParDefaut extends Filiere {
@@ -54,21 +53,24 @@ public class FiliereParDefaut extends Filiere {
 			repartitionInitiale.put(Chocolat.C_HQ_E,  30.0); // Haute Qualite   Equitable 
 			repartitionInitiale.put(Chocolat.C_MQ_E,  10.0); // Moyenne Qualite Equitable 
 			repartitionInitiale.put(Chocolat.C_MQ,    15.0); // Moyenne Qualite ni Bio ni Equitable 
-			repartitionInitiale.put(Chocolat.C_BQ,    30.0); // Basse Qualite   ni Bio ni Equitable 
+			repartitionInitiale.put(Chocolat.C_BQ_E,  10.0); // Basse Qualite   Equitable 
+			repartitionInitiale.put(Chocolat.C_BQ,    20.0); // Basse Qualite   ni Bio ni Equitable 
 			break;
 		case 1 : 
 			repartitionInitiale.put(Chocolat.C_HQ_BE, 10.0); // Haute Qualite   Bio-Equitable 
 			repartitionInitiale.put(Chocolat.C_HQ_E,  20.0); // Haute Qualite   Equitable 
 			repartitionInitiale.put(Chocolat.C_MQ_E,   5.0); // Moyenne Qualite Equitable 
 			repartitionInitiale.put(Chocolat.C_MQ,    25.0); // Moyenne Qualite ni Bio ni Equitable 
-			repartitionInitiale.put(Chocolat.C_BQ,    40.0); // Basse Qualite   ni Bio ni Equitable 
+			repartitionInitiale.put(Chocolat.C_BQ_E,  10.0); // Basse Qualite   Equitable 
+			repartitionInitiale.put(Chocolat.C_BQ,    30.0); // Basse Qualite   ni Bio ni Equitable 
 			break;
 		default : 
 			repartitionInitiale.put(Chocolat.C_HQ_BE,   6.0); // Haute Qualite   Bio-Equitable   
 			repartitionInitiale.put(Chocolat.C_HQ_E,   10.0); // Haute Qualite   Equitable   
 			repartitionInitiale.put(Chocolat.C_MQ_E,    4.0); // Moyenne Qualite Equitable 
 			repartitionInitiale.put(Chocolat.C_MQ,     25.0); // Moyenne Qualite ni Bio ni Equitable 
-			repartitionInitiale.put(Chocolat.C_BQ,     55.0); // Basse Qualite   ni Bio ni Equitable 
+			repartitionInitiale.put(Chocolat.C_BQ_E,   15.0); // Basse Qualite   Equitable 
+			repartitionInitiale.put(Chocolat.C_BQ,     40.0); // Basse Qualite   ni Bio ni Equitable 
 		}
 		this.cf = new ClientFinal(7200000.0 , repartitionInitiale, DISTRIBUTIONS_ANNUELLES);
 		this.ajouterActeur(cf);
