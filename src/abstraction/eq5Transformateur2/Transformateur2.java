873931dfca6c160f92a01a7c9605b870fa853b50
NilsRoss <nils.rossignol@imt-atlantique.net>
2025-02-25 11:03:47 +0100

    Erreur de document journal

--- src/abstraction/eq5Transformateur2/Transformateur2.java
+++ src/abstraction/eq5Transformateur2/Transformateur2.java
@@ -4,17 +4,5 @@ public class Transformateur2 extends Transformateur2Acteur  {
 	
 	public Transformateur2() {
 		super();
-		private journal;
-
-		public Transformateur2() {
-			super();
-			this.journal = setJournaux();
-		}
-
-		public void next() {
-			super.next();
-			journal.info("Étape numéro: " + this.getEtape());
-		}
-
 	}
 }
