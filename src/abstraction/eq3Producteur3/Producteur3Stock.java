e8f665d03ed4acda8974adf5f1a11f43b1f8dac1
Haurel Poilpré <zoe.poilpre@etu.u-paris.fr>
2025-02-25 10:22:41 +0100

    debut stock le vrai

--- src/abstraction/eq3Producteur3/Producteur3Stock.java
+++ src/abstraction/eq3Producteur3/Producteur3Stock.java
@@ -1,5 +1,11 @@
 package abstraction.eq3Producteur3;
 
+import abstraction.eqXRomu.general.VariablePrivee;
+
 public class Producteur3Stock extends Producteur3GestionTerrains {
+
+    protected VariablePrivee stockFeve;
+
+    
     
 }
--- src/abstraction/eq3Producteur3/Producteur3Vente.java
+++ src/abstraction/eq3Producteur3/Producteur3Vente.java
@@ -1,5 +1,63 @@
 package abstraction.eq3Producteur3;
 
-public class Producteur3Vente extends Producteur3Stock{
+import abstraction.eqXRomu.bourseCacao.IVendeurBourse;
+import abstraction.eqXRomu.contratsCadres.Echeancier;
+import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
+import abstraction.eqXRomu.produits.Feve;
+import abstraction.eqXRomu.produits.IProduit;
+
+public class Producteur3Vente extends Producteur3Stock implements IVendeurBourse, IVendeurContratCadre{
+
+
+
+
+
+
+
+    @Override
+    public double offre(Feve f, double cours) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public double notificationVente(Feve f, double quantiteEnT, double coursEnEuroParT) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void notificationBlackList(int dureeEnStep) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean vend(IProduit produit) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Echeancier contrePropositionDuVendeur(ExemplaireContratCadre contrat) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public double propositionPrix(ExemplaireContratCadre contrat) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public double livrer(IProduit produit, double quantite, ExemplaireContratCadre contrat) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
