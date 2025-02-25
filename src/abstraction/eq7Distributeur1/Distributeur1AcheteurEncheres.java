780435b6a45e53d30db1f32c532a4302dfc8045b
GUY_Maxime <maxime.guy0@yahoo.com>
2025-02-25 11:56:54 +0100

    Modif formule Volume>Quantité_Voulue

--- src/abstraction/eq7Distributeur1/Distributeur1AcheteurEncheres.java
+++ src/abstraction/eq7Distributeur1/Distributeur1AcheteurEncheres.java
@@ -22,10 +22,11 @@ public class Distributeur1AcheteurEncheres extends Distributeur1Acteur  {
 
 	public double proposerPrix(int product, double volume){
 		double price = this.priceProduct.get(product);
+		double wantedquantity = this.requiredQuantities.get(product);
 		int numberSuccessedSell = this.succesedSell.get(product);
 		Boolean soldVolumeTooHigh = tooManyVolume(product, volume);
 		if (soldVolumeTooHigh){
-			return(price*(90+10*(1-Math.exp(-1*numberSuccessedSell/5))));
+			return(price*(90+10*(1-Math.exp(-1*numberSuccessedSell/5))*(1-Math.exp((wantedquantity-volume)/1000))));
 		}
 	return(price*(1.1+0.02*numberSuccessedSell));
 	}
