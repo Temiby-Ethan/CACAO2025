e713d47d3ccd6ce52065f715506283676271505f
NilsRoss <nils.rossignol@imt-atlantique.net>
2025-02-25 10:55:29 +0100

    Journal

--- src/abstraction/eq5Transformateur2/Transformateur2.java
+++ src/abstraction/eq5Transformateur2/Transformateur2.java
@@ -4,5 +4,17 @@ public class Transformateur2 extends Transformateur2Acteur  {
 	
 	public Transformateur2() {
 		super();
+		private journal;
+
+		public Transformateur2() {
+			super();
+			this.journal = setJournaux();
+		}
+
+		public void next() {
+			super.next();
+			journal.info("Étape numéro: " + this.getEtape());
+		}
+
 	}
 }
