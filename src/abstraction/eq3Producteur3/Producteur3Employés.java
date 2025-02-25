fec8bba43ca2d7c568135ec545162a0776589534
Haurel Poilpré <zoe.poilpre@etu.u-paris.fr>
2025-02-25 11:22:50 +0100

    vente v2

--- src/abstraction/eq3Producteur3/Producteur3Employés.java
+++ src/abstraction/eq3Producteur3/Producteur3Employés.java
@@ -1,5 +1,11 @@
 package abstraction.eq3Producteur3;
 
+import abstraction.eqXRomu.produits.Feve;
+
 public class Producteur3Employés extends Producteur3Vente {
+    
+    public Producteur3Employés() {
+        super();
+    }
     
 }
