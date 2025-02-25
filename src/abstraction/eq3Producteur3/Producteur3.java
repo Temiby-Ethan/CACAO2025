2cbf363b85f1890389680365f7d097276fafb4ab
Haurel Poilpré <zoe.poilpre@etu.u-paris.fr>
2025-02-25 08:46:32 +0100

    defi Journal

--- src/abstraction/eq3Producteur3/Producteur3.java
+++ src/abstraction/eq3Producteur3/Producteur3.java
@@ -1,8 +1,24 @@
 package abstraction.eq3Producteur3;
 
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.general.Journal;
+
 public class Producteur3 extends Producteur3Acteur  {
+
+	protected Journal defiJournal = new Journal("Journal defi",this);
+	protected int nbNext = 1;
 	
 	public Producteur3() {
 		super();
 	}
+
+
+	@Override
+	public void next() {
+		defiJournal.ajouter(Filiere.LA_FILIERE.getEtape()+"\n");
+
+
+
+	}
+
 }
