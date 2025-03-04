efcd5e7db5ccf8e6dd266ab4a8ad7cc52463acac
Paul MALATY <malatypaul@gmail.com>
2025-03-04 12:10:31 +0100

    Début Contrat Cadre

--- src/abstraction/eq3Producteur3/Producteur3Vente.java
+++ src/abstraction/eq3Producteur3/Producteur3Vente.java
@@ -1,12 +1,19 @@
 package abstraction.eq3Producteur3;
 
+import java.util.List;
+
 import abstraction.eqXRomu.bourseCacao.BourseCacao;
 import abstraction.eqXRomu.bourseCacao.IVendeurBourse;
+import abstraction.eqXRomu.contratsCadres.Echeancier;
+import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
 import abstraction.eqXRomu.filiere.Filiere;
 import abstraction.eqXRomu.produits.Feve;
+import abstraction.eqXRomu.produits.Gamme;
+import abstraction.eqXRomu.produits.IProduit;
 
 
-public class Producteur3Vente extends Producteur3Stock implements IVendeurBourse{
+public class Producteur3Vente extends Producteur3Stock implements IVendeurBourse,IVendeurContratCadre{
 
    
         public Producteur3Vente() {
@@ -45,6 +52,53 @@ public class Producteur3Vente extends Producteur3Stock implements IVendeurBourse
     }
 
     // CONTRAT CADRE //
+
+    protected List<ExemplaireContratCadre> mesContratEnTantQueVendeur;
+
+    @Override
+    public boolean vend(IProduit produit) {
+        if (produit instanceof Feve) {
+            Feve feve = (Feve)produit;
+            return feve.getGamme().equals(Gamme.MQ);
+        }
+        return false;
+    }
+
+    @Override
+    public Echeancier contrePropositionDuVendeur(ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contrePropositionDuVendeur'");
+    }
+
+
+    @Override
+    public double propositionPrix(ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'propositionPrix'");
+    }
+
+
+    @Override
+    public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contrePropositionPrixVendeur'");
+    }
+
+
+    @Override
+    public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'notificationNouveauContratCadre'");
+    }
+
+
+    @Override
+    public double livrer(IProduit produit, double quantite, ExemplaireContratCadre contrat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'livrer'");
+    }
+
+
     
 
 
