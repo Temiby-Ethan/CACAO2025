45d63b6ef561328ac5450a207e7f28c9d8c149bd
ABOUROUSSE <77542232+Temiby-Ethan@users.noreply.github.com>
2025-03-04 08:24:40 +0100

    Défi 2 equipe 7

--- src/abstraction/eq7Distributeur1/Distributeur1.java
+++ src/abstraction/eq7Distributeur1/Distributeur1.java
@@ -1,22 +1,39 @@
 package abstraction.eq7Distributeur1;
 
-import abstraction.eqXRomu.filiere.Filiere;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 import abstraction.eqXRomu.general.Journal;
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.acteurs.TransformateurXActeur;
 import abstraction.eqXRomu.general.Variable;
-import abstraction.eqXRomu.produits.Chocolat;
 import abstraction.eqXRomu.produits.ChocolatDeMarque;
+import abstraction.eqXRomu.produits.Chocolat;
+import abstraction.eqXRomu.produits.Feve;
+import abstraction.eqXRomu.produits.IProduit;
 
 public class Distributeur1 extends Distributeur1Acteur  {
 	
+	// défi 1 et 2 par Alexiho
 	private Journal journal;  // Déclaration du journal
-	private Variable stockChocolat;
+	private Map<Chocolat, Double> stockChocolat; // Table de hachage pour stocker les quantités de chocolat
 
-	public Distributeur1() {
-		super();
-
-		this.journal = new Journal("Journal de EQX", this); // Initialisation du journal
-		this.stockChocolat = new Variable("Stock de chocolat", this, 0.0); 
-	}
+    public Distributeur1() { // par Alexiho
+        super();
+        
+        this.journal = new Journal("Journal de EQ7", this); // Initialisation du journal
+        this.stockChocolat = new HashMap<>();
+        
+        // Initialisation des stocks à 0.0
+        stockChocolat.put(Chocolat.C_HQ_BE, 0.0); // CHOCOLAT HAUTE QUALITE BIO EQUITABLE
+        stockChocolat.put(Chocolat.C_HQ_E, 0.0);  // CHOCOLAT HAUTE QUALITE EQUITABLE
+        stockChocolat.put(Chocolat.C_MQ_E, 0.0);  // CHOCOLAT MOYENNE QUALITE EQUITABLE
+        stockChocolat.put(Chocolat.C_MQ, 0.0);   // CHOCOLAT MOYENNE QUALITE (NI BIO NI EQUITABLE)
+        stockChocolat.put(Chocolat.C_BQ_E, 0.0);  // CHOCOLAT BASSE QUALITE EQUITABLE
+        stockChocolat.put(Chocolat.C_BQ, 0.0);   // CHOCOLAT BASSE QUALITE (NI BIO NI EQUITABLE)
+    }
 
 	public void next() // par Alexiho
 	{
@@ -29,11 +46,30 @@ public class Distributeur1 extends Distributeur1Acteur  {
         double quantiteAjoutee = 100.0; // 100 tonnes
 
         // Mettre en rayon (ajouter au stock)
-        stockChocolat.ajouter(this, quantiteAjoutee);
+        //this.stockC_MQ += quantiteAjoutee;
+
+		stockChocolat.put(Chocolat.C_MQ, stockChocolat.get(Chocolat.C_MQ) + quantiteAjoutee);
 
         // Enregistrement dans le journal
         journal.ajouter("Étape " + etape + " : Ajout de " + quantiteAjoutee + " t de " + produit + " en rayon.");
 
 		System.out.println(journal);
+		System.out.println(stockChocolat.get(Chocolat.C_MQ));
 	}
+
+	public double getQuantiteEnStock(IProduit p, int cryptogramme) { // par Alexiho
+		if (this.cryptogramme == cryptogramme) { // Vérification que l'accès est autorisé
+			return stockChocolat.get(p);
+		} else {
+			return 0; // Les acteurs non assermentés n'ont pas à connaître notre stock
+		}
+	}
+
+	// Renvoie les journaux
+	public List<Journal> getJournaux() { // par Alexiho
+		List<Journal> res=new ArrayList<Journal>();
+		res.add(journal);
+		return res;
+	}
+	
 }
