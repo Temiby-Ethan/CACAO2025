5631e4f706be1a0b27aee827da51616acd0095b6
GUY_Maxime <maxime.guy0@yahoo.com>
2025-03-04 09:33:17 +0100

    Encheres bon format

--- src/abstraction/eq7Distributeur1/Distributeur1AcheteurEncheres.java
+++ src/abstraction/eq7Distributeur1/Distributeur1AcheteurEncheres.java
@@ -1,33 +1,151 @@
 package abstraction.eq7Distributeur1;
 ///Maxime GUY///
-
+import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Distributeur1AcheteurEncheres extends Distributeur1Acteur  {
+import javax.swing.text.AttributeSet.ColorAttribute;
+
+import abstraction.eqXRomu.encheres.Enchere;
+import abstraction.eqXRomu.encheres.IAcheteurAuxEncheres;
+import abstraction.eqXRomu.encheres.MiseAuxEncheres;
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.filiere.IActeur;
+import abstraction.eqXRomu.general.Journal;
+import abstraction.eqXRomu.general.Variable;
+import abstraction.eqXRomu.produits.Chocolat;
+import abstraction.eqXRomu.produits.ChocolatDeMarque;
+import abstraction.eqXRomu.produits.IProduit;
+
+public class Distributeur1AcheteurEncheres implements IAcheteurAuxEncheres  {
 
 	protected Integer cryptogramme;
 	private List<Double> priceProduct;
 	private List<Double> requiredQuantities;
 	private List<Integer> succesedSell;
+	private String name;
+	private Color color;
+	private List<Double> stock;
 
-	public Distributeur1AcheteurEncheres(List<Double> priceProduct, List<Double> requiredQuantities) {
+	public Distributeur1AcheteurEncheres(List<Double> priceProduct, List<Double> requiredQuantities, String name, Color color, List<Double> stock) {
 		super();
 		this.priceProduct = priceProduct;
+		this.name = name ;
+		this.color = color;
 		this.requiredQuantities = requiredQuantities;
+		this.stock = stock;
 	}
 
-	public Boolean tooManyVolume(int product, double volume){
-		return(this.requiredQuantities.get(product)<volume);
+	public int getInt(ChocolatDeMarque product){
+        int idProduct = 0;
+        switch(product.getGamme()){
+            case BQ : idProduct=0;
+            case MQ : idProduct=2;
+            case HQ : idProduct=4;
+        }
+        if (product.isBio()){
+            idProduct++;
+        }
+        if (product.isEquitable()){
+            idProduct++;
+        }
+        return(idProduct);
+    }
+
+	public Boolean tooManyVolume(ChocolatDeMarque product, double volume){
+		int idProduct = getInt(product);
+		return(this.requiredQuantities.get(idProduct)<volume);
 	}
 
-	public double proposerPrix(int product, double volume){
-		double price = this.priceProduct.get(product);
-		double wantedquantity = this.requiredQuantities.get(product);
-		int numberSuccessedSell = this.succesedSell.get(product);
-		Boolean soldVolumeTooHigh = tooManyVolume(product, volume);
-		if (soldVolumeTooHigh){
-			return(price*(90+10*(1-Math.exp(-1*numberSuccessedSell/5))*(1-Math.exp((wantedquantity-volume)/1000))));
+	public double proposerPrix(MiseAuxEncheres encheres){
+		IProduit product = encheres.getProduit();
+		if (product instanceof ChocolatDeMarque) {
+        	ChocolatDeMarque chocolat = (ChocolatDeMarque) product;
+			double volume = encheres.getQuantiteT();
+			int idProduct = getInt(chocolat);
+			double price = this.priceProduct.get(idProduct);
+			double wantedquantity = this.requiredQuantities.get(idProduct);
+			int numberSuccessedSell = this.succesedSell.get(idProduct);
+			if (tooManyVolume(chocolat, volume)){
+				return(price*(90+10*(1-Math.exp(-1*numberSuccessedSell/5))*(1-Math.exp((wantedquantity-volume)/1000))));
+			}
+		return(price*(1.1+0.02*numberSuccessedSell));
 		}
-	return(price*(1.1+0.02*numberSuccessedSell));
+		return(0);
 	}
+	public void initialiser(){
+
+	}
+
+	public void notifierAchatAuxEncheres(Enchere enchereRetenue){
+
+	}
+	public void notifierEnchereNonRetenue(Enchere enchereNonRetenue){
+		
+	};
+
+	public String getNom(){
+		return(this.name);
+	}
+
+	public Color getColor(){
+		return(this.color);
+	}
+
+	public String getDescription(){
+		return("Acheteur aux encheres de l'equipe 7");
+	}
+
+	public void next(){
+
+	}
+
+	public List<Variable> getIndicateurs(){
+		List<Variable> indicateurs = new ArrayList<Variable>();
+		return(indicateurs);
+	}
+
+	public List<Variable> getParametres(){
+		List<Variable> parametres = new ArrayList<Variable>();
+		return(parametres);
+	}
+
+	public List<Journal> getJournaux(){
+		List<Journal> journaux = new ArrayList<Journal>();
+		return(journaux);
+	}
+
+	public void setCryptogramme(Integer crypto){
+		this.cryptogramme = crypto;
+	}
+
+	public void notificationFaillite(IActeur acteur){
+
+	}
+
+	public void notificationOperationBancaire(double montant){
+
+	}
+
+	public List<String> getNomsFilieresProposees(){
+		List<String> noms = new ArrayList<String>();
+		return(noms);
+	}
+
+	public Filiere getFiliere(String nom){
+		Filiere test = new Filiere();
+		return(test);
+	}
+
+	public double getQuantiteEnStock(IProduit p, int cryptogramme ){
+		if (this.cryptogramme == cryptogramme){
+			if (p instanceof ChocolatDeMarque){
+				ChocolatDeMarque chocolat = (ChocolatDeMarque) p;
+				return(stock.get((int) getInt(chocolat)));
+			}
+			return(0);
+		}
+		return(0);
+	}
+	
 }
