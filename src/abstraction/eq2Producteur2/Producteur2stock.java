d599ce798a11d20fbe932db5e2fd53e7b7b45d63
m24phili <maxime.philippon@imt-atlantique.net>
2025-03-11 08:30:26 +0100

    crea classe terrain et stock

---
+++ src/abstraction/eq2Producteur2/Producteur2stock.java
@@ -0,0 +1,11 @@
+package abstraction.eq2Producteur2;
+
+import abstraction.eqXRomu.general.Journal;
+
+public class Producteur2stock extends Producteur2VendeurBourse {
+    
+    private Journal suiviStock;
+    protected int cryptogramme;
+    private double coutStockage;
+    
+}
