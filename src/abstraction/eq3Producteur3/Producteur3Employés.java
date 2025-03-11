4385caef533943e83334c2fd0a6f9d1612b7f253
Paul MALATY <malatypaul@gmail.com>
2025-03-11 10:48:59 +0100

    Ajout de la masse salariale

--- src/abstraction/eq3Producteur3/Producteur3Employés.java
+++ src/abstraction/eq3Producteur3/Producteur3Employés.java
@@ -2,10 +2,19 @@ package abstraction.eq3Producteur3;
 
 import abstraction.eqXRomu.produits.Feve;
 
+import java.util.ArrayList;
 public class Producteur3Employés extends Producteur3Vente {
     
     public Producteur3Employés() {
         super();
     }
     
+    private double getMasseSalariale(){
+        double masseSalariale = 0;
+        double[] salaires = {6,15,37.5};
+        for (int i = 0; i < 3; i++){
+            masseSalariale += effectifs[i] * salaires[i];
+        return masseSalariale;
+        }
+    }
 }
