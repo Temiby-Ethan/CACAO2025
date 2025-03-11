83e02fb6c3d57da437d8e18aecbd49a1f9cad764
SimonBecand <simon.becand@imt-atlantique.net>
2025-03-11 10:05:50 +0100

    contratcadre

--- src/abstraction/eq5Transformateur2/Transformateur2.java
+++ src/abstraction/eq5Transformateur2/Transformateur2.java
@@ -1,6 +1,6 @@
 package abstraction.eq5Transformateur2;
 
-public class Transformateur2 extends DistributeurChocolatDeMarque {
+public class Transformateur2 extends Stock {
 
     public Transformateur2(){
         super();
