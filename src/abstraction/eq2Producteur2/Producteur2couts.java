caae288ec49f114db02aa84323e5dff16f978463
m24phili <maxime.philippon@imt-atlantique.net>
2025-03-18 10:17:51 +0100

    crea cout

--- src/abstraction/eq2Producteur2/Producteur2couts.java
+++ src/abstraction/eq2Producteur2/Producteur2couts.java
@@ -1,12 +1,50 @@
+//Maxime Philippon
 package abstraction.eq2Producteur2;
 
 public class Producteur2couts extends Producteur2stock {
+
+    private double cout_tot;
+    private double cout_unit_F_BQ;
+    private double cout_unit_F_BQ_E;
+    private double cout_unit_F_MQ;
+    private double cout_unit_F_MQ_E;
+    private double cout_unit_F_HQ_E;
+    private double cout_unit_F_HQ_BE;
+    private double prix_F_BQ;
+    private double prix_F_BQ_E;
+    private double prix_F_MQ;
+    private double prix_F_MQ_E;
+    private double prix_F_HQ_E;
+    private double prix_F_HQ_BE;
     
-    private double coutFixe;
-    private double coutVariable;
-    private double coutTotal;
-    private double coutUnitaire;
-    private double coutUnitaireStock;
-    private double coutUnitaireSechage
+    public Producteur2couts() {
+        super();
+        this.cout_tot = 0;
+        this.cout_unit_F_BQ = 0;
+        this.cout_unit_F_BQ_E = 0;
+        this.cout_unit_F_MQ = 0;
+        this.cout_unit_F_MQ_E = 0;
+        this.cout_unit_F_HQ_E = 0;
+        this.cout_unit_F_HQ_BE = 0;
+        this.prix_F_BQ = 0;
+        this.prix_F_BQ_E = 0;
+        this.prix_F_MQ = 0;
+        this.prix_F_MQ_E = 0;
+        this.prix_F_HQ_E = 0;
+        this.prix_F_HQ_BE = 0;
+    }
+
+    public void initialiser() {
+        super.initialiser();
+    }
+
+    public void next() {
+        super.next();
+        calculCout();
+    }
+
+    public void calculCout() {
+        
+    }
     
 }
