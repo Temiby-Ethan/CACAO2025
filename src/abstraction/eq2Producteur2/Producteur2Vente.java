558d1f9ec5a875902ce9d0ba8598a735309ef5e1
Thomas <thomas.brun@imt-atlantique.net>
2025-03-18 11:20:13 +0100

    fichier cadre

---
+++ src/abstraction/eq2Producteur2/Producteur2Vente.java
@@ -0,0 +1,17 @@
+package abstraction.eq2Producteur2;
+
+import abstraction.eqXRomu.bourseCacao.BourseCacao;
+import abstraction.eqXRomu.bourseCacao.IVendeurBourse;
+import abstraction.eqXRomu.contratsCadres.Echeancier;
+import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.produits.Feve;
+import abstraction.eqXRomu.produits.Gamme;
+import abstraction.eqXRomu.produits.IProduit;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Producteur2Vente extends Producteur2Stock implements IVendeurBourse, IVendeurContratCadre{
