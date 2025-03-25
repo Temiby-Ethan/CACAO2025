b6f7c4f5297d3e074f538b2c204b1ad6f21049d6
SimonBecand <simon.becand@imt-atlantique.net>
2025-03-25 11:21:28 +0100

    nvx

--- src/abstraction/eq5Transformateur2/DecisionsActeur.java
+++ src/abstraction/eq5Transformateur2/DecisionsActeur.java
@@ -9,29 +9,48 @@ import abstraction.eqXRomu.produits.Feve;
 import abstraction.eqXRomu.produits.IProduit;
 import java.util.HashMap;
 import java.util.List;
+import javax.crypto.spec.OAEPParameterSpec;
+import abstraction.eqXRomu.filiere.Banque;
 
-public class DecisionsActeur extends MarqueChocolat{
+
+public class DecisionsActeur extends ProcessChoco{
     
-
-    private double quantiteAProduire; // en tablette à chaque step
-    private HashMap<Chocolat, Double> proportion; // proportion de chaque chocolat dans la production totale
+    private HashMap<Feve, Double> proportion; // proportion de chaque chocolat dans la production totale
+    private double objectifChangementProportion;
     
 
     public DecisionsActeur(){
         super();
-        this.quantiteAProduire=Math.min(this.capaciteProductionEmployes(),this.getCapaciteProductionMachines());
-        this.proportion = new HashMap<Chocolat, Double>();
-        }
-    
+        this.proportion = new HashMap<Feve, Double>();
+        this.proportion.put(Feve.F_MQ, 0.4);
+        this.proportion.put(Feve.F_MQ_E, 0.4);
+        this.proportion.put(Feve.F_HQ_E, 0.1);
+        this.proportion.put(Feve.F_HQ_BE, 0.1);
 
-    
-
-
-    public void approvisionnementEnFeve(){
-        
+        this.objectifChangementProportion = 10.0 *10000000000.00; // augmentation de 10% de notre capitale de départ en euros
     }
 
+    public double getProductionTotale(){
+        return Math.min(super.getCapaciteProductionMachines() ,super.capaciteProductionEmployes());
+    }
+    
+    public void ChangementProportion(){
+        if(this.proportion.get(Chocolat.C_MQ)>1){
+            this.proportion.put(Feve.F_MQ, this.proportion.get(Chocolat.C_MQ)-0.05);
+            this.proportion.put(Feve.F_MQ_E, this.proportion.get(Chocolat.C_MQ_E)-0.05);
+            this.proportion.put(Feve.F_HQ_E, this.proportion.get(Chocolat.C_HQ_E)+0.05);
+            this.proportion.put(Feve.F_HQ_BE, this.proportion.get(Chocolat.C_HQ_BE)+0.05);
 
+        }
+    }
+
+    public boolean objecifChangementProportion(){
+        if(Filiere.LA_FILIERE.getBanque().getSolde(this,super.cryptogramme)>=this.objectifChangementProportion){
+            this.objectifChangementProportion=this.objectifChangementProportion*1.1;
+            return true;
+        }
+        return false;
+    }
 
     public void next(){
         super.next();
