9ee1ddbbdd7c7f5b470feafe5268de4fa635ce71
ABOUROUSSE <77542232+Temiby-Ethan@users.noreply.github.com>
2025-04-15 11:37:09 +0200

    Ajouts de journaux

--- src/abstraction/eq7Distributeur1/Distributeur1.java
+++ src/abstraction/eq7Distributeur1/Distributeur1.java
@@ -26,6 +26,8 @@ public class Distributeur1 extends Distributeur1AcheteurAppelOffre implements ID
 	// défi 1 et 2 par Alexiho
 	protected Journal journal; // Déclaration du journal
 	protected Journal journalE;  // Déclaration du journal
+	protected Journal journalCC; // Déclaration du journal
+	protected Journal journalAO; // Déclaration du journal
 	// protected Map<ChocolatDeMarque, Variable> stocksChocolats; // Table de hachage pour stocker les quantités de chocolat
 	//protected List<ChocolatDeMarque> chocolats;
 	protected List<Double> prix;
@@ -44,7 +46,9 @@ public class Distributeur1 extends Distributeur1AcheteurAppelOffre implements ID
 		super();
         
         this.journal = new Journal("Journal stock de EQ7", this); // Initialisation du journal
-		this.journalE = new Journal("Journal de vente de EQ7", this); // Initialisation du journal
+		this.journalE = new Journal("Journal d'enchères de EQ7", this); // Initialisation du journal
+		this.journalCC = new Journal("Journal contrat cadre de EQ7", this);
+		this.journalAO = new Journal("Journal contrat cadre de EQ7", this);
 
 		
 		predictionsVentesPourcentage = Arrays.asList(3.6 , 3.6 , 5.0 , 3.6 , 3.6 , 3.6 , 3.6 , 7.0 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 3.6 , 13.0);
@@ -114,15 +118,21 @@ public class Distributeur1 extends Distributeur1AcheteurAppelOffre implements ID
         //journal.ajouter("Étape " + etape + " : Ajout de " + quantiteAjoutee + " t de " + produit + " en rayon.");
 		
 		String str_journal_stock = "";
-		String str_journal_vente = "";
+		String str_journal_E = "";
+		String str_journal_CC = "";
+		String str_journal_AO = "";
 
 		for (int i = 0 ; i<chocolats.size() ; i++){
 			str_journal_stock += this.stocksChocolats.get(chocolats.get(i)).getNom() + " = " + this.stocksChocolats.get(chocolats.get(i)).getValeur() + " ; ";
-			str_journal_vente += "a";
+			str_journal_E += "a";
+			str_journal_CC += "b";
+			str_journal_AO += "c";
 		}
 
 		journal.ajouter(str_journal_stock);
-		journalE.ajouter(str_journal_vente);
+		journalE.ajouter(str_journal_E);
+		journalCC.ajouter(str_journal_CC);
+		journalAO.ajouter(str_journal_AO);
 
 		// définition des capacités de ventes
 
@@ -216,6 +226,8 @@ public class Distributeur1 extends Distributeur1AcheteurAppelOffre implements ID
 		List<Journal> res=new ArrayList<Journal>();
 		res.add(journal);
 		res.add(journalE);
+		res.add(journalCC);
+		res.add(journalAO);
 		return res;
 	}
 	
