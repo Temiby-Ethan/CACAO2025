4b5cc350668e47e7f4bb34780ba3ec423352e0c7
YAOU Reda <yaoureda24@gmail.com>
2025-05-06 08:55:00 +0200

    pertePer

--- src/abstraction/eq4Transformateur1/Transformateur1Stocks.java
+++ src/abstraction/eq4Transformateur1/Transformateur1Stocks.java
@@ -738,8 +738,8 @@ public class Transformateur1Stocks extends Transformateur1Usine implements IFabr
 	private void pertePeremption(double[] peremptionArray, IProduit p, Color color, Journal journalPer) {
 		int n = peremptionArray.length - 1;
 		if (peremptionArray[n] > 0) {
-			this.retirerDuStock(p, peremptionArray[n], this.cryptogramme);
 			journalPer.ajouter(Color.pink, color, "Péremption: On retire "+peremptionArray[n]+ " tonnes de "+p+" de notre stock");
+			this.retirerDuStock(p, peremptionArray[n], this.cryptogramme);
 		}
 
 		for (int i=n; i>=1; i--) {
