d50fba70b960e94a82cfdd902a181299949d3881
SimonBecand <simon.becand@imt-atlantique.net>
2025-05-06 10:10:36 +0200

    changement demande burse

--- src/abstraction/eq5Transformateur2/AcheteurBourse.java
+++ src/abstraction/eq5Transformateur2/AcheteurBourse.java
@@ -14,12 +14,15 @@ public class AcheteurBourse extends ContratCadreAcheteur implements IAcheteurBou
    
     public double demande(Feve f, double cours) {
         if (f == Feve.F_HQ_BE || f == Feve.F_HQ_E || f == Feve.F_MQ_E || f == Feve.F_MQ){
+            
             double enStock = super.getQuantiteStock(f);
-            double quantiteVoulueProduction= super.getProductionTotale()*super.getProportion(f);
+            double quantiteVoulueProduction= super.getProductionTotale()*super.getProportion(f)/10000; // en tonnes
+            this.journal.ajouter("feve"+f+"productionTotale: " + super.getProductionTotale() + " proportion " + super.getProportion(f));
             double difference= quantiteVoulueProduction-enStock;
-
+            this.journal.ajouter("enStock: " + enStock + " quantiteVoulueProduction: " + quantiteVoulueProduction + " difference: " + difference);
             double solde = Filiere.LA_FILIERE.getBanque().getSolde(this, super.cryptogramme);
-        double montantMax = solde / cours;
+            double montantMax = solde / cours;
+            this.journal.ajouter("solde: " + solde + " montantMax: " + montantMax);
 
         if (difference > 0 && difference <= montantMax) {
         return difference;
