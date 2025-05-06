2a3cdda8db9bed0db6dafad710c9a3ffa89e62fc
ABOUROUSSE <77542232+Temiby-Ethan@users.noreply.github.com>
2025-05-06 10:17:01 +0200

    changement contrat cadre avec getstepfin et getstepdebut

--- src/abstraction/eq7Distributeur1/Distributeur1AcheteurContratCadre.java
+++ src/abstraction/eq7Distributeur1/Distributeur1AcheteurContratCadre.java
@@ -94,23 +94,22 @@ public class Distributeur1AcheteurContratCadre extends Distributeur1Stock implem
 			echeancierActuel = listeEcheancier.get(listeEcheancier.size()-1);
 		}
 		for (int step = echeancierActuel.getStepDebut(); step<=echeancierActuel.getStepFin() ; step++){
-
-			for (int i = 0; i < step; i++){
+			for (int i = 0; i < (echeancierActuel.getStepFin()-echeancierActuel.getStepDebut()+1); i++){
 				valeurtotale += echeancierActuel.getQuantite(i);
 			}
-			
 			double quantiteDemandee = echeancierActuel.getQuantite(step);
 			double quantiteVoulue = requiredQuantities.get(cdmToInt(chocolat))/predictionsVentesPourcentage.get(echeancierActuel.getStepDebut()%24)*predictionsVentesPourcentage.get(step%24);
-			if (quantiteDemandee > quantiteVoulue*(1+0.02*step)){
-				echeancierActuel.set(step, Math.max(Math.max(100,quantiteVoulue*(1+0.02*step)), valeurtotale/(10*step)));
+			if (quantiteDemandee > quantiteVoulue*(1+0.02*tour)){
+				echeancierActuel.set(step, Math.max(Math.max(100,quantiteVoulue*(1+0.02*tour)), valeurtotale/(10*(echeancierActuel.getStepFin()-echeancierActuel.getStepDebut()))));
 			}
-			if (quantiteDemandee < quantiteVoulue*(1-0.02*step)){
-				echeancierActuel.set(step, Math.max(Math.max(100,quantiteVoulue*(1-0.02*step)), valeurtotale/(10*step)));
+			if (quantiteDemandee < quantiteVoulue*(1-0.02*tour)){
+				echeancierActuel.set(step, Math.max(Math.max(100,quantiteVoulue*(1-0.02*tour)), valeurtotale/(10*(echeancierActuel.getStepFin()-echeancierActuel.getStepDebut()))));
 			}
 			if (quantiteDemandee < 100){
-				echeancierActuel.set(step, Math.max(100, 100 + valeurtotale/(10*step)));
+				echeancierActuel.set(step, Math.max(100, 100 + valeurtotale/(10*(echeancierActuel.getStepFin()-echeancierActuel.getStepDebut()))));
 			}
 			valeurtotale = 0;
+			
 		}
 		return(echeancierActuel);
 	}
