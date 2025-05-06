61fd990cd0bb117b481b4b53c26abd5f6be9fe3f
adrienb03 <VotreEmail@example.com>
2025-05-06 10:31:02 +0200

    push

--- src/abstraction/eq1Producteur1/Producteur1Couts.java
+++ src/abstraction/eq1Producteur1/Producteur1Couts.java
@@ -2,27 +2,28 @@ package abstraction.eq1Producteur1;
 
 import abstraction.eqXRomu.general.Journal;
 
-public class Producteur1Couts extends Employes {
+public class Producteur1Couts {
 
-    protected Journal journal;
-    protected Producteur1 Producteur1; // Référence au Producteur1
+    private Journal journal;
+    private Producteur1 producteur1; 
+    private Employes employes; 
 
-    public Producteur1Couts() {
-        this.journal = new Journal("Journal Coûts", Producteur1);
-
-        
+    public Producteur1Couts(Producteur1 producteur1, Employes employes) {
+        this.producteur1 = producteur1;
+        this.employes = employes;
+        this.journal = new Journal("Journal Coûts", producteur1);
     }
 
-
-    public double calculerCoutEntretien(){
-        double coutTotal = 0.0;
-        coutTotal += (enfants * coutEnfant) + (adultesNonFormes * coutAdulteNonForme) + (adultesFormes * coutAdulteForme);
-        return coutTotal;
+    // Coût total d'entretien lié aux employés
+    public double calculerCoutEmployes() {
+        return employes.calculerCoutEntretien();
     }
 
-    public void next(){
-        double coutEntretien = calculerCoutEntretien();
-        journal.ajouter("Coût d'entretien total par step : " + coutEntretien);
+    // Enregistrement les coûts
+    public void next() {
+        double cout = calculerCoutEmployes();
+        journal.ajouter("Coût total des employés ce step : " + cout);
     }
 
 }
+
