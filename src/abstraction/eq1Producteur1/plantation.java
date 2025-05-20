925feb2161533bde87511af52120910a16f2b334
Célian Tissier <celian.tissier@outlook.com>
2025-05-20 08:28:29 +0200

    ?

---
+++ src/abstraction/eq1Producteur1/plantation.java
@@ -0,0 +1,27 @@
+package abstraction.eq1Producteur1;
+
+
+
+import abstraction.eqXRomu.general.Journal;
+
+// Amal Moncer
+public class plantation {
+
+    protected String typedeplant;
+    protected int nombre_arbes;
+    protected int temps_avant_production; // en steps
+    protected int duree_de_vie = 960; // en steps
+    protected int besoin_main_doeuvre;
+    protected int production_par_arbre;
+    protected int nb_feves_par_cabosse;
+    protected double poids_feve_par_cabosse_apres_sechage;
+    protected int temps_de_pousse;
+    protected int temps_de_sechage;
+    protected double prix_achat;
+    protected int prix_replantation;
+    protected Journal journal;
+    protected int nombre_hectares;
+    protected int nombre_feves;
+    protected int nombre_feves_total;
+
+    }
